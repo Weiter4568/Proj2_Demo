471f62cb9bb07d10ca2635fccb125ef5d617bf0a
Weiter4568 <90168326+Weiter4568@users.noreply.github.com>
2023-05-30 17:44:02 +0800

    API implement V1

--- src/main/java/cse/java2/project/Application.java
+++ src/main/java/cse/java2/project/Application.java
@@ -2,11 +2,13 @@ package cse.java2.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * This is the main class of the Spring Boot application.
  */
 @SpringBootApplication
+@EnableJpaRepositories
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/cse/java2/project/JSON_Model/Q5.java
@@ -0,0 +1,24 @@
+package cse.java2.project.JSON_Model;
+
+import java.util.List;
+
+public class Q5 {
+    List<String> name;
+    List<Integer> value;
+
+    public List<String> getName() {
+        return name;
+    }
+
+    public void setName(List<String> name) {
+        this.name = name;
+    }
+
+    public List<Integer> getValue() {
+        return value;
+    }
+
+    public void setValue(List<Integer> value) {
+        this.value = value;
+    }
+}
